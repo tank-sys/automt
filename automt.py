e61e999858e284713e2d58f0087038dbcf639a45
Tank <62957183+tank-sys@users.noreply.github.com>
2020-11-04 19:43:22 +0700

    Add files via upload

--- automt.py
+++ automt.py
@@ -3,6 +3,7 @@ import paramiko
 import subprocess
 from datetime import datetime
 
+#perintah mikrotiknya
 perintah = """
 / system identity pr
 / ip addr pr
@@ -16,14 +17,14 @@ upass = ""
 fld = "semuaip"
 
 isilog = ""
-
+os.system("color")
 def helppage():
-    print(color.BOLD+"Penggunaan :"+color.END)
-    print(" "+color.BOLD+"python automt.py"+color.END)
+    print(color.BOLD+"Penggunaan :")
+    print(" "+color.BOLD+"python automt.py")
     print(" Default. Alamat ip akan membaca seluruh alamat yang ada di file semuaip.")
-    print(" "+color.BOLD+"python automt.py withlog"+color.END)
+    print(" "+color.BOLD+"python automt.py withlog")
     print(" Membuat file log : python automt.py ")
-    print(" "+color.BOLD+"python automt.py ip {ipaddress}"+color.END)
+    print(" "+color.BOLD+"python automt.py ip {ipaddress}")
     print(" Menjalankan perintah pada satu host (ipaddress)")
 
 def main():
@@ -45,16 +46,16 @@ def main():
         elif sys.argv[1] == ("-h"):
             helppage()
         else:
-            print(color.RED+"error : withlog atau ip {ipaddress}"+color.END)
+            print(color.RED+"error : withlog atau ip {ipaddress}")
     elif len(sys.argv)==3:
         if sys.argv[1] == ("ip"):
             verifdevice(sys.argv[2])
         else:
-            print(color.RED+"cara : python automt.py ip {ipaddress}"+color.END)
+            print(color.RED+"cara : python automt.py ip {ipaddress}")
     elif len(sys.argv)==1:
         md()
     else:
-        print(color.RED+"error gak jelas : coba dibaca hasilnya python automt.py -h "+color.END)
+        print(color.RED+"error gak jelas : coba dibaca hasilnya python automt.py -h ")
 
 def md():
     if os.path.exists(fld):
@@ -69,8 +70,7 @@ def md():
             dd = device.split("|")
             ldc = len(dd)
             print("+----------------------------------+")
-            print("+ "+str(i)+". IP Device : "+color.CYAN+dd[0]+color.END)
-            print("+----------------------------------+")
+            print("+ "+color.BOLD+str(i)+". IP Device : "+color.CYAN+dd[0])
             if ldc != 1:
                 print("## Coba dibaca lagi formatnya dari semuaip!")
             else:
@@ -84,22 +84,23 @@ def verifdevice(ipaddr):
     try:
 #        print("## Ping device....")
 #        subprocess.check_output(['ping', '-c', '2', '-W', '1', ipaddr],stderr=subprocess.STDOUT,universal_newlines=True)
-#        print(color.BLUE+"## Device UP. Njajal connect ke device....."+color.END)
+#        print(color.BLUE+"## Device UP. Njajal connect ke device.....")
         eksekusi(ipaddr, uport, uname, upass)
     except:
-        print(color.RED+"## Device Down. Lewatin aja....."+color.END)
+        print(color.RED+"## Device Down. Lewatin aja.....")
 
 def eksekusi(ipaddr, uport, uname, upass):
     cssh = paramiko.SSHClient()
     cssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
         cssh.connect(ipaddr, uport, uname, upass)
-        print(color.GREEN+"Success connect ke device."+color.END)
-        print(color.UNDERLINE+color.RED+"Hasilnya :"+color.END)
+        print(color.GREEN+"Success connect ke device.")
+        print(color.RED+"Hasilnya :" + color.END)
         stdin, stdout, stderr = cssh.exec_command(format(perintah))
-        print(stdout.read())
+        hsl = (stdout.read())
+        print(hsl.decode('utf8'))
     except:
-        print(color.RED+"Gak connect ke device. Lewatin....."+color.END)
+        print(color.RED+"Gagal connect ke device. Lewatin.....")
         pass
 
 class color:
